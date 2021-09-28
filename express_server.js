3831996f3ebf7a4d74479b90f6d11008ffd13db9
charuzain <charuabhizain@gmail.com>
2021-09-27 23:48:53 -0400

    Initial Commit

--- express_server.js
+++ express_server.js
@@ -12,7 +12,10 @@ app.get("/", (req, res) => {
 });
 app.get("/urls.json", (req , res)=>{
   res.json(urlDatabase); //sends a response ie)parameter converted to a JSON string using the JSON.stringify() method
-})
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
