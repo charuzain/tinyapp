bbc8b637fecef5704308e9ef2d2ce709ca604de8
charuzain <charuabhizain@gmail.com>
2021-09-28 19:57:31 -0400

    Second route & Template added

--- express_server.js
+++ express_server.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
-
-
-
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-app.get("/urls.json", (req , res)=>{
-  res.json(urlDatabase); //sends a response ie)parameter converted to a JSON string using the JSON.stringify() method
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+ 
+  res.render("urls_show", templateVars);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
