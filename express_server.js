690064398bf359fe8ef6e8e1dd8045f645f09a51
charuzain <charuabhizain@gmail.com>
2021-09-29 01:23:53 -0400

    Url shortening

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const generateRandomString = function() {
-  let randomString = Math.random().toString(36).substring(2,8);
+  let randomString = Math.random().toString(36).substring(2, 8);
   return randomString;
 };
 
@@ -22,23 +22,33 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new",(req,res)=>{
+app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.post("/urls", (req, res) => {
+  const shortuRL = generateRandomString();
+  urlDatabase[shortuRL] = req.body.longURL;
+  res.redirect(`/urls/${shortuRL}`);
+
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL  = urlDatabase[shortURL];
+  res.redirect(longURL);
+
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
- 
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res)=>{
-  console.log(req.body);
-  res.send("ok");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
