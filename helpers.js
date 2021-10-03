c28ba0bbe7fb2d7e7abda0e6f486dce192dd8ea0
charuzain <charuabhizain@gmail.com>
2021-10-02 23:29:52 -0400

    helper module :eslinted & comments added

--- helpers.js
+++ helpers.js
@@ -1,7 +1,6 @@
 // Helper Functions ------------------------------------------------
 
-
-
+// look up user by their email id
 const findUserByEmail = function(email , userDb) {
   for (const userId in userDb) {
     const user = userDb[userId];
@@ -12,19 +11,18 @@ const findUserByEmail = function(email , userDb) {
   //return false;
 };
 
-
+// adds a new user to user database
 const createUser = function(email , password , userDb) {
   const userId = generateRandomString();
-
   userDb[userId] = {
     id: userId,
     email:email,
     password:password
   };
   return userId;
-
 };
 
+// find the urls created by a user
 const urlsForUser = function(id, urlDb) {
   let loggedInUserURL = {};
   for (const urlId in urlDb) {
@@ -35,7 +33,7 @@ const urlsForUser = function(id, urlDb) {
   return loggedInUserURL;
 };
 
-
+// generate a radom shortURL
 const generateRandomString = function() {
   let randomString = Math.random().toString(36).substring(2, 8);
   return randomString;
